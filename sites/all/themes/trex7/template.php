31a79a521176a9ebda1c829ed3529dccc8d289c3
Jeroen Budts <jeroen@andoria>
2012-05-01 19:52:32 +0200

    theme: use site slogan when no breadcrumb

--- sites/all/themes/trex7/template.php
+++ sites/all/themes/trex7/template.php
@@ -4,6 +4,6 @@ function trex7_breadcrumb(&$variables) {
   if (!empty($variables) && count($variables['breadcrumb']) > 1) {
     return '<div class="breadcrumb">' . implode(' » ', $variables['breadcrumb']) . '</div>';
   } else {
-    return '<div class="breadcrumb">' . l('"Living and coding on the 3 dimensional brane of the multiverse"', '<front>')  . '</div>';
+    return '<div class="breadcrumb">' . l(variable_get('site_slogan', ''), '<front>')  . '</div>';
   }
 }
