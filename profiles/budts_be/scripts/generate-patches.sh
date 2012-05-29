259d198c741107ea4b85072d93975ec4d92c3ada
Jeroen Budts <jeroen@andoria>
2012-05-29 23:12:19 +0200

    tagadelic: patch for php notice fix

--- profiles/budts_be/scripts/generate-patches.sh
+++ profiles/budts_be/scripts/generate-patches.sh
@@ -11,3 +11,6 @@ git diff daa7b35 783e6a7 .gitignore > profiles/budts_be/patches/gitignore.patch
 
 # generate patch for .htaccess
 git diff 15cadf0 5817a77 .htaccess > profiles/budts_be/patches/htaccess.patch
+
+# generate patch for tagadelic: fix a php notice
+git diff cd99231 fc22009 sites/all/modules/contrib/tagadelic/tagadelic.module > profiles/budts_be/patches/tagadelic-1435238-fix-php-notice.patch
