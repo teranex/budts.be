c75c02921b6f9ecc9bd73a397486496b1acea62b
Jeroen Budts <jeroen@andoria>
2013-01-27 13:59:45 +0100

    patch: in .htaccess for openid

--- profiles/budts_be/scripts/generate-patches.sh
+++ profiles/budts_be/scripts/generate-patches.sh
@@ -12,6 +12,7 @@ git diff fce99d7 087eb49 .htaccess > profiles/budts_be/patches/2-htaccess.patch
 git diff a9e68e0 6f884d0 .htaccess > profiles/budts_be/patches/3-htaccess.patch
 git diff 4a70244 7ce5ec8 .htaccess > profiles/budts_be/patches/4-htaccess.patch
 git diff 404073d 0349a31 .htaccess > profiles/budts_be/patches/5-htaccess.patch
+git diff abf599b 38a9932 .htaccess > profiles/budts_be/patches/6-htaccess.patch
 
 # generate patch for tagadelic: fix a php notice
 git diff cd99231 fc22009 sites/all/modules/contrib/tagadelic/tagadelic.module > profiles/budts_be/patches/tagadelic-1435238-fix-php-notice.patch
